b05cc6020b119c5d55b49cedb7c40fa3f22c8288
HengyuMeng <84671272+HengyuMeng@users.noreply.github.com>
2026-08-13 14:59:43 +0000

    Deploying to gh-pages from @ HengyuMeng/HengyuMeng.github.io@8628d9dfeba37fd7dc9bd7d534b2e1480eac2acf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,12 +16,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+        },{id: "nav-exhibitions",
+          title: "exhibitions",
+          description: "selected artworks, installations, and exhibitions.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
+            window.location.href = "/exhibitions/";
           },
         },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
       
